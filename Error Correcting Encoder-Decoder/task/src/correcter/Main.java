9f18d913fe1d080bee3daab2d067e7c2265d9732
Foxtam <foxtam7@ya.ru>
2020-09-28 09:04:04 +0300

    Symbol-level correction code

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -10,14 +10,57 @@ public class Main {
 
     static final Random generator = new Random();
 
+    static final int mistakeWindowWidth = 3;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        String lineWithMistakes = makeMistakes(line, 3);
-        System.out.println(lineWithMistakes);
+        String userMessage = scanner.nextLine();
+        System.out.println(userMessage);
+
+        String tripledMsg = triplingAllSymbols(userMessage);
+        System.out.println(tripledMsg);
+
+        String msgWithMistakes = makeMistakes(tripledMsg);
+        System.out.println(msgWithMistakes);
+
+        String decodedMsg = decodeIncorrectMsg(msgWithMistakes);
+        System.out.println(decodedMsg);
     }
 
-    static String makeMistakes(String source, int mistakeWindowWidth) {
+    static String decodeIncorrectMsg(String msg) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < msg.length(); i += mistakeWindowWidth) {
+            char baseChar = getBaseSymbol(msg.substring(i, i + mistakeWindowWidth));
+            builder.append(baseChar);
+        }
+        return builder.toString();
+    }
+
+    static char getBaseSymbol(String text) {
+        char[] charArray = text.toCharArray();
+        for (char ch : charArray) {
+            if (countSymbol(charArray, ch) > 1) return ch;
+        }
+        throw new IllegalStateException();
+    }
+
+    static int countSymbol(char[] charArray, char symbol) {
+        int counter = 0;
+        for (char ch : charArray) {
+            if (symbol == ch) counter++;
+        }
+        return counter;
+    }
+
+    static String triplingAllSymbols(String text) {
+        StringBuilder builder = new StringBuilder();
+        for (char ch : text.toCharArray()) {
+            builder.append(String.valueOf(ch).repeat(mistakeWindowWidth));
+        }
+        return builder.toString();
+    }
+
+    static String makeMistakes(String source) {
         StringBuilder builder = new StringBuilder(source);
         for (int i = 0; i < builder.length() / mistakeWindowWidth * mistakeWindowWidth; i += mistakeWindowWidth) {
             int randomOffset = generator.nextInt(mistakeWindowWidth);
